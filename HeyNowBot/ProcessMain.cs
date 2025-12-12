a238c7fe07df10ddaf3bf6b6d2d7d1137d05188d
sykim <sykim@broadcns.com>
2025-12-12 15:56:55 +0900

    main수정

---
+++ HeyNowBot/ProcessMain.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HeyNowBot.Service;
+
+namespace HeyNowBot
+{
+    public class ProcessMain
+    {
+        public ProcessMain()
+        {
+               
+        }
+        public async void Run()
+        {
+            Console.WriteLine("Hello, World!");
+            await TelegramService.Instance.SendMessageAsync("Hello from HeyNowBot!");
+            await Task.Delay(30000);
+        }
+    }
+}
--- HeyNowBot/Program.cs
+++ HeyNowBot/Program.cs
@@ -1,10 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using HeyNowBot.Service;
+using System;
 
-
-while (true)
+namespace MyApp
 {
-    Console.WriteLine("Hello, World!");
-    await TelegramService.Instance.SendMessageAsync("Hello from HeyNowBot!");
-    await Task.Delay(30000);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            await TelegramService.Instance.SendMessageAsync("Hello from HeyNowBot!");
+            await Task.Delay(30000);
+        }
+    }
 }
