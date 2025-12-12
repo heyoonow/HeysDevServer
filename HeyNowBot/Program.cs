2b7ef2ac7b2fc0e7a19faa6c549b44cda4b62d0f
sykim <sykim@broadcns.com>
2025-12-12 14:50:25 +0900

    telegram test

--- HeyNowBot/Program.cs
+++ HeyNowBot/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using HeyNowBot.Service;
+
+
+while (true)
+{
+    Console.WriteLine("Hello, World!");
+    await TelegramService.Instance.SendMessageAsync("Hello from HeyNowBot!");
+    await Task.Delay(30000);
+}
---
+++ HeyNowBot/Service/ServiceTelegram.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace HeyNowBot.Service
+{
+    public interface ITelegramService
+    {
+        Task SendMessageAsync(string text);
+    }
+    public class TelegramService : ITelegramService
+    {
+        private const string _botToken = "8439410251:AAEnbnXVmQfzJTNg9PF8Ik8V7q7mVLnCJoo";
+        private const long _chatId = 7747196424;
+        private static readonly Lazy<TelegramService> _instance =new Lazy<TelegramService>(() => new TelegramService());
+        private readonly TelegramBotClient _bot;
+        public static TelegramService Instance => _instance.Value;
+
+        private TelegramService()
+        {
+            _bot = new TelegramBotClient(_botToken);
+        }
+        public async Task SendMessageAsync(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+                return;
+
+            try
+            {
+                await _bot.SendMessage(
+                    chatId: _chatId,
+                    text: text
+                );
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[TelegramService] 전송 오류: {ex.Message}");
+            }
+        }
+    }
+}
