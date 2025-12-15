db839a7cec89744158db2de3357f690c5c7e2920
김상윤 <heyoonow@gmail.com>
2025-12-15 20:29:04 +0900

    ㅇㅇ

--- HeyNowBot/ProcessMain.cs
+++ HeyNowBot/ProcessMain.cs
@@ -23,7 +23,7 @@ namespace HeyNowBot
         {
             await SetLoadAsync();
             _timeChekerService = new TimeChekerService();
-            await _bot.SendMessageAsync("[HeyNowBot] 봇이 시작되었습니다.111");
+            await _bot.SendMessageAsync("[HeyNowBot] 봇이 시작되었습니다.11231");
 
             _timeChekerService.OnHourReached += async (hour)  =>
             {
